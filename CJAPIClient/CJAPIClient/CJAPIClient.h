b52f4c1e89a0475deecb8ef49db5c423ec1fb3de
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-04-14 18:07:37 +0100

    Initial commit

---
+++ CJAPIClient/CJAPIClient/CJAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  CJAPIClient.h
+//  CJAPIClient
+//
+//  Created by Bruno Abrantes on 14/04/14.
+//  Copyright (c) 2014 Clubjudge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CJAPIClient : NSObject
+
+@end
---
+++ CJAPIClient/Pods/Pods-CJAPIClientTests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CJAPIClientTests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_CJAPIClientTests_Kiwi
+@end
