02c8112e53055a0adcfada27e322876f0f24d29a
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-04-22 15:38:50 +0100

    Implements the CJLinksInfo model
    Used for collection traversing and accessing subresources

---
+++ CJAPIClient/CJAPIClient/Models/CJLinksInfo.h
@@ -0,0 +1,28 @@
+//
+//  CJLinksInfo.h
+//  CJAPIClient
+//
+//  Created by Bruno Abrantes on 22/04/14.
+//  Copyright (c) 2014 Clubjudge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CJAPIRequest.h"
+
+@interface CJLinksInfo : NSObject
+
+@property (readonly) NSDictionary *links;
+
+#pragma mark - Initialisation
+- (instancetype)initWithInfo:(NSDictionary *)info;
+
+#pragma mark - Request
+- (CJAPIRequest *)requestForLink:(NSString *)link;
+
+@end
+
+#define kLinksFirst     @"first"
+#define kLinksPrevious  @"previous"
+#define kLinksSelf      @"self"
+#define kLinksNext      @"next"
+#define kLinksLast      @"last"
