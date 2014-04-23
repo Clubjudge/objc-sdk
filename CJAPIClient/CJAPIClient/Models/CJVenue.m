f57ee1039dd8378285c2baba1708fc6ad256ffc8
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-04-23 18:59:18 +0100

    Implements Venue Model

---
+++ CJAPIClient/CJAPIClient/Models/CJVenue.m
@@ -0,0 +1,64 @@
+//
+//  CJVenue.m
+//  CJAPIClient
+//
+//  Created by Bruno Abrantes on 23/04/14.
+//  Copyright (c) 2014 Clubjudge. All rights reserved.
+//
+
+#import "CJVenue.h"
+#import "CJModel+Following.h"
+#import "NSDate+StringParsing.h"
+#import "CJEvent.h"
+
+@implementation CJVenue
+
+- (instancetype)initWithInfo:(NSDictionary *)info
+{
+  self = [super initWithInfo:info];
+  if (self && info) {
+    // Core properties
+    _name = info[kVenueName];
+    _description = info[kVenueDescription];
+    _updatedAt = [NSDate dateWithISO8601String:info[kVenueUpdatedAt]];
+    _following = [info[kVenueFollowing] boolValue];
+    _followersCount = info[kVenueFollowersCount];
+    _commentsCount = info[kVenueCommentsCount];
+    _socialMentionsCount = info[kVenueSocialMentionsCount];
+    _reviewCount = info[kVenueReviewCount];
+    _socialLinks = info[kVenueSocialLinks];
+    _geolocation = info[kVenueGeolocation];
+    _background = info[kVenueBackground];
+    _address = info[kVenueAddress];
+    _logos = info[kVenueLogos];
+    _email = info[kVenueEmail];
+    _websiteURL = info[kVenueWebsiteURL];
+    _phoneNumber = info[kVenuePhoneNumber];
+    
+    // Embeddables
+    // TODO: proper mapping to models
+    _events = [(NSArray *) info[kVenueEvents] map:^id(NSDictionary *event) {
+      return [[CJEvent alloc] initWithInfo:event];
+    }];
+    _recentEvents = [(NSArray *) info[kVenueRecentEvents] map:^id(NSDictionary *event) {
+      return [[CJEvent alloc] initWithInfo:event];
+    }];
+    _upcomingEvents = [(NSArray *) info[kVenueUpcomingEvents] map:^id(NSDictionary *event) {
+      return [[CJEvent alloc] initWithInfo:event];
+    }];
+    _followers = [NSArray arrayWithArray:info[kVenueFollowers]];
+  }
+  return self;
+}
+
+- (void)follow
+{
+  [self followEntity:@"venue"];
+}
+
+- (void)unfollow
+{
+  [self unfollowEntity:@"venue"];
+}
+
+@end
