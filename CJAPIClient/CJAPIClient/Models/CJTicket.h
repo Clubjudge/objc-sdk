2eb51b81df19e1e968a9bd09f4788cba22300d8e
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-04-24 22:52:03 +0100

    Implements the Ticket Model

---
+++ CJAPIClient/CJAPIClient/Models/CJTicket.h
@@ -0,0 +1,24 @@
+//
+//  CJTicket.h
+//  CJAPIClient
+//
+//  Created by Bruno Abrantes on 24/04/14.
+//  Copyright (c) 2014 Clubjudge. All rights reserved.
+//
+
+#import "CJModel.h"
+
+@interface CJTicket : CJModel
+
+#pragma mark - Core properties
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSNumber *cents;
+@property (nonatomic, strong) NSString *currency;
+@property (nonatomic, strong) NSString *text;
+
+@end
+
+#define kTicketName @"name"
+#define kTicketCents @"cents"
+#define kTicketCurrency @"currency"
+#define kTicketText @"text"
