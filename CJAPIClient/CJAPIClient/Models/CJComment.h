703d27ed863a94004dcc95b61714bb2c96f1ba3b
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-04-24 17:14:48 +0100

    Implements the CJComment model

---
+++ CJAPIClient/CJAPIClient/Models/CJComment.h
@@ -0,0 +1,31 @@
+//
+//  CJComment.h
+//  CJAPIClient
+//
+//  Created by Bruno Abrantes on 24/04/14.
+//  Copyright (c) 2014 Clubjudge. All rights reserved.
+//
+
+#import "CJModel.h"
+@class CJUser;
+
+typedef enum {
+  kCommentTypeComment,
+  kCommentTypeActivity,
+} CommentType;
+
+@interface CJComment : CJModel
+
+#pragma mark - Core properties
+@property (nonatomic, strong) NSString* Id;
+@property (nonatomic, strong) NSString *message;
+@property (nonatomic, strong) NSDate *createdAt;
+@property (nonatomic, assign) CommentType type;
+@property (nonatomic, strong) CJUser *user;
+
+@end
+
+#define kCommentMessage @"message"
+#define kCommentCreatedAt @"timestamp"
+#define kCommentType @"type"
+#define kCommentUser @"user"
