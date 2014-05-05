2ea47ca5b3537d435dcf05a4ae3d0305caaedf64
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-05-05 09:46:03 +0100

    Documents categories

--- CJAPIClient/Categories/CJModel+Following.h
+++ CJAPIClient/Categories/CJModel+Following.h
@@ -6,11 +6,22 @@
 //  Copyright (c) 2014 Clubjudge. All rights reserved.
 //
 
+/**
+ CJModel+Following is a category on CJModel that adds follow/unfollow capabilities to the target model.
+ */
+
 #import "CJModel.h"
 
 @interface CJModel (Following)
 
+/**
+ Follows a given entity, ie. "artist"
+ */
 - (void)followEntity:(NSString *)entity;
+
+/**
+ Unfollows a given entity, ie. "artist"
+ */
 - (void)unfollowEntity:(NSString *)entity;
 
 @end
